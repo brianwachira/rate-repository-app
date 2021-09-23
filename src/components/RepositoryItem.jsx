0a9d8a67d5b8529ae58a598de2b7a17c03a6e944
brianwachira <brianwachira7@gmail.com>
2021-09-23 23:11:51 +0300

    [FIX] : correct margin property

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ import { View,Text } from 'react-native';
 const RepositoryItem = ({item}) => {
 
     return (
-        <View style={{margin: '0'}}>
+        <View style={{margin: 0}}>
             <Text style={{ color: 'green', fontSize:24, fontWeight:'700'}}>Full Name : {item.fullName}</Text>
             <Text>Description : {item.description}</Text>
             <Text>Language : {item.language}</Text>
