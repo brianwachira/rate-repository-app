d08f262956e397b42ed6d6dfb19aa5a2c0da435f
brianwachira <bw@Eupotechs-MacBook-Air.local>
2021-09-29 11:01:29 +0300

    [ENHANCEMENT] : use button instead

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,16 +1,37 @@
 import React from "react";
-import { Pressable,View } from "react-native";
+import { Pressable,View, StyleSheet, Button } from "react-native";
 import FormikTextInput from "./FormikTextInput";
-import Text from './Text';
+import theme from '../theme';
 
+const styles = StyleSheet.create({
+
+    input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: '#bfbfbf',
+    padding: 10,
+    borderRadius: 6
+    },
+    button: {
+        backgroundColor: '#0165d4'
+
+    }
+});
 const SignInForm = ({ onSubmit }) => {
     return (
-        <View>
-            <FormikTextInput name="Username" placeholder="Username"/>
-            <FormikTextInput name="Password" placeholder="Password" secureTextEntry={true}/>
-            <Pressable onPress={onSubmit}>
+        <View style={theme.container}>
+            <FormikTextInput name="Username" placeholder="Username" style={styles.input}/>
+            <FormikTextInput name="Password" placeholder="Password" secureTextEntry={true} style={styles.input}/>
+            {/* <Pressable onPress={onSubmit}>
                 <Text>Sign In</Text>
-            </Pressable>
+            </Pressable> */}
+            <Button
+                style={styles.button}
+                onPress={onSubmit}
+                title="Sign In"
+                accessibilityLabel="Manage your account"
+            />
         </View>
     );
 };
